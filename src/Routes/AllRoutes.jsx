1bb83cc02d61c2bdda5bb5bc54ee3aef4b37cc76
arjundangi01 <arjundangi907473@gmail.com>
2023-09-18 15:34:27 +0530

    done4

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -19,7 +19,7 @@ const AllRoutes = () => {
       <Route path="/signup" element={<Signup/>} >Signup</Route>
       <Route path="/profile" element={<ProfilePrivateRoute> <Profile/> </ProfilePrivateRoute>} >Profile</Route>
       <Route path="/tour" element={<Tour/>} >Tour Page</Route>
-      <Route path="/tour/:id" element={<TourDetail/>} >Tour Detail Page</Route>
+      <Route path="/tour/tour/:id" element={<TourDetail/>} >Tour Detail Page</Route>
       <Route path="/payment" element={<PaymentPrivateRoute> <Payment/> </PaymentPrivateRoute>}  >Payment Page</Route>
       <Route path="/payment/confirm" element={<PaymentConfirm/>} >Payment Confirm Page</Route>
     </Routes>
