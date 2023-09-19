e6c205e38dee22fd010bb03bf5d2340feb29f87f
arjundangi01 <arjundangi907473@gmail.com>
2023-09-19 11:11:18 +0530

    done login

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,17 +1,14 @@
-import { legacy_createStore, combineReducers } from "redux";
+import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import { totalAmountReducer } from "./TotalAmount/totalAmountReducer";
 import { tourReducer } from "./Tour Data/tourReducer";
 import { userReducer } from "./User Data/userReducer";
 import { tourDetailReducer } from "./Tour Detail/tourDetailReducer";
-import sessionReducer from "./sessionReducer";
-
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-  sessionReducer,
   totalAmountReducer,
   tourReducer,
   userReducer,
   tourDetailReducer,
 });
- export const store = legacy_createStore(rootReducer);
-// export const store = legacy_createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
